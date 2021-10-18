6d248f991bf373923c699520c6e155e12406f622
MAXIM RUGAL <rugalek94@gmail.com>
2021-10-18 19:42:17 +0300

    add: header, homepage, coinpage

---
+++ cryptodash/src/CryptoContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("USD");
+  const [symbol, setSymbol] = useState("$");
+  useEffect(() => {
+    if (currency === "USD") setSymbol("$");
+    else if (currency === "EUR") setSymbol("€");
+    else if (currency === "BYN") setSymbol("Br");
+  }, [currency]);
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </Crypto.Provider>
+  );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
